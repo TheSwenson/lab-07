dbf6777d4d80453ff4210dbfc2a4abb2ec4aca8c
Jacob Swenson <jswens912@gmail.com>
2019-08-21 11:28:16 -0500

    weather shit

---
+++ server.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-console */
+'use strict';
+const superagent = require('superagent');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+app.get('/location', (request, response) => {
+  try {
+   superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODEAPI_KEY}`)
+   .then((geoData) => {
+     const location = new Location(request.query.data, geoData.body);
+     response.send(location);
+   });
+    
+  } catch(error) {
+    response.status(500).send('Dis website is broke. Call someone who cares.');
+  }
+});
+
+function Location(query, geoData){
+  this.search_query = query;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+function Weather(weatherData, i) {
+  this.forecast = weatherData.daily.data[i].summary;
+  this.time = convertTime();
+  function convertTime() {
+    return new Date(weatherData.daily.data[i].time * 1000).toString().split(' ').slice(0, 4).join(' ');
+  }
+}
+
+app.get('/weather', (request, response) => {
+  try {
+    const url = `https://api.darksky.net/forecast/${process.env.DARKSKYAPI_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+    return superagent.get(url)
+      .then(result => {
+        const weatherSummaries = result.body.daily.data.map(day => {
+          return new Weather(day);
+        });
+        response.send(weatherSummaries);
+      })
+
+      } catch(error) {
+          response.status(500).send('Dis website is broke. Call someone who cares.');
+      } 
+      
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('I know that you came to party baby, baby, baby, baby');
+});
